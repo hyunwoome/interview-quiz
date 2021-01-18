44ea72f5a2ffd266518011c240a0e45289befee7
hyunwoome <hyunwoome@icloud.com>
2021-01-18 20:38:33 +0900

    Add Main UI

--- src/components/sections/Main.jsx
+++ src/components/sections/Main.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import MainStyle from './Main.module.css';
 
 function Main() {
-	return <div className={MainStyle.main}></div>;
+	return (
+		<div className={MainStyle.main}>
+			<div className={MainStyle.innerMain}>
+				<div className={MainStyle.question}>
+					<div className={MainStyle.questionText}>
+						<p>Lorem ipsum dolor sit, </p>
+					</div>
+				</div>
+				<div className={MainStyle.answer}>
+					<div className={MainStyle.answerText}>
+						<p>Lorem ipsum dolor sit, </p>
+					</div>
+				</div>
+				<div className={MainStyle.utils}>유틸창</div>
+			</div>
+		</div>
+	);
 }
 
 export default Main;
